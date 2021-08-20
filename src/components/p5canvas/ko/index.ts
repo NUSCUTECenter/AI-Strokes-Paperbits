6cdde3bdfdc29eec08cebccd8241eb4f05681e12
Francis Lee <francis@u.nus.edu>
2021-08-20 10:33:30 +0800

    Remove unnecessary ko files for p5canvas component

--- src/components/p5canvas/ko/index.ts
+++ src/components/p5canvas/ko/index.ts
@@ -1,5 +1,3 @@
 export * from "../p5Canvas.publish.module";
 export * from "../p5Canvas.design.module";
 export * from "./p5CanvasEditor";
-export * from "./p5CanvasViewModel";
-export * from "./p5CanvasViewModelBinder";
